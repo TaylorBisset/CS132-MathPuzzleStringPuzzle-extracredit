84fffdb41705117bc7d0629fed52c94a42393d3b
Taylor Bisset <taylorbisset@gmail.com>
2023-10-05 12:13:57 -0700

    main.cpp renamed and comments spaced out for clarity.

--- CS132-MathPuzzleStringPuzzle-extracredit/main.cpp
+++ CS132-MathPuzzleStringPuzzle-extracredit/main.cpp
@@ -25,10 +25,14 @@ nzgskfaaovhzivufm
 EvCC Math Awareness Month
 
 The title is not a typo!  It is an example of an encrypted message and is also this week's puzzle for Math Awareness Month.  
+
 To decode the message it will be helpful to know how it was encrypted.  
+
 All upper case letters were replaced with lower case letters and spaces were removed.  
+
 After that every letter was substituted with the opposite letter in the alphabet, 
 meaning a was replaced with z, b was replaced with y, c was replaced with x, d was replaced with w, and so on.  
+
 Can you reverse this process and decode the message?
 
 Worth 5 points of Program extra credit.   
