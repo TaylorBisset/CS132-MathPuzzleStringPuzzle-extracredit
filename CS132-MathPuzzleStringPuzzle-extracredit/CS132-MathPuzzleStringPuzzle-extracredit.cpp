c6006395d7cb8323ddea48d78b92da8f6c474f3c
Taylor Bisset <taylorbisset@gmail.com>
2023-10-05 12:10:40 -0700

    Program header commented in.

--- CS132-MathPuzzleStringPuzzle-extracredit/CS132-MathPuzzleStringPuzzle-extracredit.cpp
+++ CS132-MathPuzzleStringPuzzle-extracredit/CS132-MathPuzzleStringPuzzle-extracredit.cpp
@@ -1,3 +1,15 @@
+/*
+ Name:          Taylor Bisset
+ Section:       A
+ Program Name:  CS132-MathPuzzleStringPuzzle-extracredit
+
+ Description:   Message decoding program. 
+                Encription details below. 
+ 
+ Repo:          https://github.com/TaylorBisset/CS132-MathPuzzleStringPuzzle-extracredit
+ */
+
+// main.cpp
 
 #include <iostream>
 
